08334d1fb52c77f5974ae628b143f1f6a5a4b441
Thomas Stangl <thomas.stangl@evopro-ag.de>
2019-10-08 10:34:48 +0200

    Save json to file; Scrollbar

--- TwinCatAdsTool.Gui/ViewModels/BackupViewModel.cs
+++ TwinCatAdsTool.Gui/ViewModels/BackupViewModel.cs
@@ -3,6 +3,7 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
 using ReactiveUI;
 using TwinCAT;
@@ -15,13 +16,15 @@ namespace TwinCatAdsTool.Gui.ViewModels
     {
         private readonly IClientService clientService;
         private readonly IPersistentVariableService persistentVariableService;
+        private readonly IProcessingService processingService;
         private string backupText;
         private readonly Subject<JObject> variableSubject = new Subject<JObject>();
 
-        public BackupViewModel(IClientService clientService, IPersistentVariableService persistentVariableService)
+        public BackupViewModel(IClientService clientService, IPersistentVariableService persistentVariableService, IProcessingService processingService)
         {
             this.clientService = clientService;
             this.persistentVariableService = persistentVariableService;
+            this.processingService = processingService;
         }
 
         public override void Init()
@@ -35,12 +38,28 @@ namespace TwinCatAdsTool.Gui.ViewModels
 
             Read = ReactiveCommand.CreateFromTask(ReadVariables, canExecute:clientService.ConnectionState.Select(state => state == ConnectionState.Connected))
                 .AddDisposableTo(Disposables);
+
+            Save = ReactiveCommand.CreateFromTask(SaveVariables, clientService.ConnectionState.Select(state => state == ConnectionState.Connected)).AddDisposableTo(Disposables);
         }
 
         private async Task<Unit> ReadVariables()
         {
             var persistentVariables = await persistentVariableService.ReadPersistentVariables(clientService.Client);
             variableSubject.OnNext(persistentVariables);
+            return Unit.Default;
+        }
+
+        private async Task<Unit> SaveVariables()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Json|*.json";
+            saveFileDialog1.Title = "Save an Json File";
+            var result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK || result == DialogResult.Yes)
+            {
+                //await processingService.Connect(PlcAddress, 851);
+                await processingService.Process(saveFileDialog1.FileName);
+            }
             return Unit.Default;
         }
 
@@ -56,5 +75,6 @@ namespace TwinCatAdsTool.Gui.ViewModels
         }
 
         public ReactiveCommand<Unit,Unit> Read { get; set; }
+        public ReactiveCommand<Unit, Unit> Save { get; set; }
     }
 }
