b865c5701899d966e936917129b275b61c1a28e1
Federico Barresi <federico.barresi@evopro-ag.de>
2020-10-26 22:45:52 +0100

    fixed tree view template

---
+++ TwinCatAdsTool.Gui/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TwinCatAdsTool.Gui.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public Visibility IfTrue { get; set; } = Visibility.Visible;
+        public Visibility IfFalse { get; set; } = Visibility.Collapsed;
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool) value ? IfTrue : IfFalse;
+            }
+
+            return DependencyProperty.UnsetValue;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
